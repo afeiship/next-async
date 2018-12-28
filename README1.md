fdbb283a7894ba0c63fc98b6fb79cacc704ec10a
afeiship <afeiship@gmail.com>
2018-12-28 23:19:45 +0800

    add: refactor init

---
+++ README1.md
@@ -0,0 +1,12 @@
+# next-async
+> Async/parallel for next
+
+## install:
+```bash
+npm install -S afeiship/next-async --registry=https://registry.npm.taobao.org
+```
+
+## usage:
+```js
+//DOCS here!
+```
--- src/next-async.js
+++ src/next-async.js
@@ -1,34 +1,15 @@
 (function () {
 
-  var global = global || window || self || this;
+  var global = global || this || window || Function('return this')();
   var nx = global.nx || require('next-js-core2');
 
-  var NxAsync = nx.declare('nx.Async', {
-    statics: {
-      parallel: function (inTasks, inCallback) {
-        var tasks = inTasks || [];
-        var callback = inCallback || nx.noop;
-        var count = tasks.length;
-        var results = [];
+  nx.async = function () {
+    //code goes here.
+  };
 
-        tasks.forEach(function (task, index) {
-          task(function (err, data) {
-            results[index] = data;
-            if (err) {
-              callback && callback(err);
-              callback = null;
-            }
-            if (--count === 0 && callback) {
-              callback(null, results);
-            }
-          });
-        });
-      }
-    }
-  });
 
   if (typeof module !== 'undefined' && module.exports) {
-    module.exports = NxAsync;
+    module.exports = nx.async;
   }
 
 }());
