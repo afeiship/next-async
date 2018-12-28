5bfc97e6e8e7b3028240adea189f595cbf909492
afeiship <afeiship@gmail.com>
2018-12-28 23:32:51 +0800

    feat: reaname

--- src/next-async.js
+++ src/next-async.js
@@ -2,7 +2,7 @@
   var global = global || this || window || Function('return this')();
   var nx = global.nx || require('next-js-core2');
 
-  var NxAsync = nx.declare('nx.Async', {
+  var NxAsync = nx.declare('nx.WeappParallel', {
     statics: {
       map: {},
       mq: [],
